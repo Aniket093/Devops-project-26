5edd2fc9f59e44d2e6ceac3006f08cd321cab464
Aniket093 <gopaleaniket@gmail.com>
2026-05-16 15:39:25 +0530

    verifying the remote git repo code

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,13 @@
 terraform {
-  required_version = ">= 1.5.0" # provided terraform version
+  required_version = ">= 1.5.0"                     # provided terraform version
 
   backend "s3" {
-    bucket         = "aniket-ecs-terraform-state" #BUCKET NAME STATED IN BACKEND/main.tf
+    bucket         = "aniket-ecs-terraform-state"   #BUCKET NAME STATED IN BACKEND/main.tf
     key            = "ecs-project/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
+   # dynamodb_table = "terraform-state-lock" # <-- Deprecated
+    use_lockfile   = true                            #added as per new update[]
+    profile      = "default"                         #explicitly added due to pipeline failure 
   }
 
   required_providers {
